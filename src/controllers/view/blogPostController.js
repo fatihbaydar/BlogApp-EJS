b0264a3e64c03326327b439a26d259bd708c9e1e
fatihbaydar <fatihbaydar2004@gmail.com>
2025-02-24 23:59:47 +0300

    components

--- src/controllers/view/blogPostController.js
+++ src/controllers/view/blogPostController.js
@@ -5,13 +5,8 @@ const BlogPost = require("../../models/blogPostModel");
 
 module.exports = {
   list: async (req, res) => {
-    const data = await res.getModelList(BlogPost,{}, "blogCategoryId");
-    res.status(200).send({
-      error: false,
-      count: data.length,
-      details: await res.getModelListDetails(BlogPost),
-      result: data,
-    });
+    const data = await res.getModelList(BlogPost, {}, "blogCategoryId");
+    res.render("index")
   },
 
   create: async (req, res) => {
