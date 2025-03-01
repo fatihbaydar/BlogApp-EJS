6833a46b9c9411b02381a058d4055ed980a70818
fatihbaydar <fatihbaydar2004@gmail.com>
2025-03-01 14:40:22 +0300

    postRead

--- src/controllers/view/blogPostController.js
+++ src/controllers/view/blogPostController.js
@@ -39,14 +39,11 @@ module.exports = {
   read: async (req, res) => {
     // req.params.postId
     // const data = await BlogPost.findById(req.params.postId)
-    const data = await BlogPost.findOne({ _id: req.params.postId }).populate(
+    const post = await BlogPost.findOne({ _id: req.params.postId }).populate(
       "blogCategoryId",
     ); // get Primary Data
 
-    res.status(200).send({
-      error: false,
-      result: data,
-    });
+    res.render("postRead", { post })
   },
 
   update: async (req, res) => {
